381ae0014de9a17a8fea5318664cf2a27e18ead8
Lukman Kurniawan <campione47@gmail.com>
2017-04-22 03:28:07 +0700

    Add files via upload

---
+++ siak.sql
@@ -0,0 +1,65 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.1.21-MariaDB : Database - siak
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`siak` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `siak`;
+
+/*Table structure for table `mahasiswa` */
+
+DROP TABLE IF EXISTS `mahasiswa`;
+
+CREATE TABLE `mahasiswa` (
+  `nim` int(11) DEFAULT NULL,
+  `nama` char(20) DEFAULT NULL,
+  `tgl_lahir` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `mahasiswa` */
+
+insert  into `mahasiswa`(`nim`,`nama`,`tgl_lahir`) values (1001,'lukman','1994-01-27'),(1002,'Dimas','1994-05-24');
+
+/*Table structure for table `matakuliah` */
+
+DROP TABLE IF EXISTS `matakuliah`;
+
+CREATE TABLE `matakuliah` (
+  `kd_mk` int(11) DEFAULT NULL,
+  `nama_matkul` char(20) DEFAULT NULL,
+  `sks` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `matakuliah` */
+
+insert  into `matakuliah`(`kd_mk`,`nama_matkul`,`sks`) values (1,'Java',3),(2,'PPA2',3);
+
+/*Table structure for table `nilai` */
+
+DROP TABLE IF EXISTS `nilai`;
+
+CREATE TABLE `nilai` (
+  `nim` int(11) DEFAULT NULL,
+  `kd_mk` int(11) DEFAULT NULL,
+  `nilai` int(11) DEFAULT NULL,
+  `huruf` char(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `nilai` */
+
+insert  into `nilai`(`nim`,`kd_mk`,`nilai`,`huruf`) values (1001,2,90,NULL),(1002,1,40,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
